cdbf8934d5fbc7d73d262567c7d9f094a9f4102a
Keshani15 <147650428+Keshani15@users.noreply.github.com>
2024-01-09 22:39:16 +0200

    added the GitHub url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This was coded by Keshani Naidoo
+        <a
+          href="https://github.com/Keshani15/react-weather-app-final"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
