fa67f52cb600353ef441ced671c0c9023a0be0ab
Keshani15 <147650428+Keshani15@users.noreply.github.com>
2024-01-10 15:02:45 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This was coded by Keshani Naidoo
-        <a
-          href="https://github.com/Keshani15/react-weather-app-final"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <button className="btn btn-primary">Search</button>
+        <h1>Weather app</h1>
+        <footer>
+          This was coded by Keshani Naidoo
+          <a
+            href="https://github.com/Keshani15/react-weather-app-final"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
