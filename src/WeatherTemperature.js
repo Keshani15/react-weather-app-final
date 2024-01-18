c23af1ebea49a170bf8f81462773c2de33dd1709
Keshani15 <147650428+Keshani15@users.noreply.github.com>
2024-01-18 22:18:33 +0200

    created useEffect for the forecast to be updated

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,42 +1,10 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFah(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCel(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={showFah}>
-            °F
-          </a>{" "}
-        </span>
-      </div>
-    );
-  } else {
-    let fahTemp = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahTemp)}</span>
-        <span className="unit">
-          <a href="/" onClick={showCel}>
-            °C
-          </a>{" "}
-          | °F{" "}
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
